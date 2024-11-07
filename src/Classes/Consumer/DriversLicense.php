9a884db94c14b5a5fc960685cb7caecb95f5595a
Chris O'Brien <chris@chrisob.com>
2024-11-06 23:15:58 -0800

    Add expiration date formatting.

--- src/Classes/Consumer/DriversLicense.php
+++ src/Classes/Consumer/DriversLicense.php
@@ -18,17 +18,54 @@ class DriversLicense
     protected function format(): void
     {
         $this->state = $this->formatState($this->state);
+        $this->expiration = $this->formatExpiration($this->expiration);
     }
 
     /**
      * @throws Php700CreditRequestException
      */
-    protected function formatState(string $state): string
+    protected function formatState(?string $state): ?string
     {
-        if (strlen($state) !== 2) {
+        if (is_string($state) && strlen($state) !== 2) {
             throw new Php700CreditRequestException('State must only be two characters.');
         }
 
         return $state;
+    }
+
+    /**
+     * @throws Php700CreditRequestException
+     */
+    protected function formatExpiration(?string $expiration): ?string
+    {
+        if (is_null($expiration)) {
+            return null;
+        }
+
+        $patterns = [
+            'MM.DD.YY' => '/^\d{2}\.\d{2}\.\d{2}$/',
+            'MM.DD.YYYY' => '/^\d{2}\.\d{2}\.\d{4}$/',
+            'MM-DD-YY' => '/^\d{2}-\d{2}-\d{2}$/',
+            'MM-DD-YYYY' => '/^\d{2}-\d{2}-\d{4}$/',
+            'MM/DD/YY' => '/^\d{2}\/\d{2}\/\d{2}$/',
+            'MM/DD/YYYY' => '/^\d{2}\/\d{2}\/\d{4}$/',
+        ];
+
+        $patternFound = false;
+
+        foreach ($patterns as $pattern) {
+            if (preg_match($pattern, $expiration)) {
+                $patternFound = true;
+                break;
+            }
+        }
+
+        if (! $patternFound) {
+            throw new Php700CreditRequestException(
+                'Expiration must match one of the following patterns: '.implode(', ', array_keys($patterns))
+            );
+        }
+
+        return $expiration;
     }
 }
